03ed0d8d8b458bb6b2a7214969ba2b41837f406e
Oduad Virgilio Aragón Cruz <oduadaragon@gmail.com>
2020-07-22 14:09:34 -0500

    Modificación con notaciones

--- src/main/java/mx/products/products/entity/Product.java
+++ src/main/java/mx/products/products/entity/Product.java
@@ -1,14 +1,34 @@
 package mx.products.products.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//Vamos a hacerla persistida en la BD
+
+@Entity														// Es la más importante
+@Table(name = "product")									// Dice vs qué tabla de la BD mapea
 public class Product {
 	
-	String name;
-	Long id;
-	//
+	@Id
+	@Column(name ="id")										// Vs qué columna mapea
+	@GeneratedValue(strategy=GenerationType.IDENTITY)		// Se incrementa del lado de la BD
+	private Long id;
 	
-	public Product(Long id,String name){
+	@Column(name ="name", nullable = false, length = 100)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String name;	
+	
+	@Column(name = "price", nullable = false)
+	private Double price;
+	
+	public Product(Long id,String name, Double price){
 		this.id = id;
 		this.name = name;
+		this.price = price;
 	}
 	
 	public Product(){
