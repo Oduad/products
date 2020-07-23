8ce12e3174901e78056b0ade4450c77a40ffc5af
Oduad Virgilio Aragón Cruz <oduadaragon@gmail.com>
2020-07-23 08:32:44 -0500

    Modificación de los 5 métodos de ProductController para que mapeen a tablas de la  BD

--- src/main/java/mx/products/products/controller/ProductController.java
+++ src/main/java/mx/products/products/controller/ProductController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,63 +15,56 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import mx.products.products.entity.Product;
+import mx.products.products.repository.ProductRepository;
 
 @RestController
 public class ProductController {
 
-	private List<Product> products = new ArrayList<>();
-	
+	@Autowired // Inyecta el bean (instancia creada en ProductRepository)
+	private ProductRepository productRepo;
+
+	/*private List<Product> products = new ArrayList<>();
+
 	public ProductController() {
 		for (int c = 0; c < 10; c++) {
-			products.add(new Product(c + 1L, "Product " + (c + 1L),100D*(1+c)));
+			products.add(new Product(c + 1L, "Product " + (c + 1L), 100D * (1 + c)));
 		}
+	}*/
+
+	@GetMapping(value = "/products")
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> products = productRepo.findAll();
+		return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
 	}
-	
-	@GetMapping(value ="/products")
-	public List<Product> findAll(){
-		return this.products;
+
+	@PostMapping(value = "/products")
+	public ResponseEntity<Product> create(@RequestBody Product product) {
+		Product newProduct = productRepo.save(product);
+		return new ResponseEntity<Product>(newProduct, HttpStatus.CREATED);
 	}
-	
-	@PostMapping(value ="/products")
-	 public Product create(@RequestBody Product product) {//-->Lo que viene en la carga útil se inyecta en esta variable.
-		//Va a hacer algo llamado margering, todos los valores del JSon los trata de casar en la clase Producto
-		this.products.add(product);
-		return product;
+
+	@PutMapping(value = "/products")
+	public ResponseEntity<Product> update(@RequestBody Product product) {
+		Product existProduct = productRepo.findById(product.getId())
+				.orElseThrow(() -> new RuntimeException("This product does not exist."));
+		existProduct.setName(product.getName());
+		existProduct.setPrice(product.getPrice());
+		productRepo.save(existProduct);
+		return new ResponseEntity<Product>(existProduct, HttpStatus.OK);
 	}
-	
-	@PutMapping(value ="/products")
-		public Product update(@RequestBody Product product) {
-			 for (Product prod: this.products) {
-				if (prod.getId().longValue()== product.getId().longValue()) {
-					prod.setName(product.getName());
-					return prod;
-				}
-			}
-			 throw new RuntimeException("This product does not exist");
+
+	@GetMapping(value = "/products/{productId}")
+	public ResponseEntity findById(@PathVariable("productId") Long productId) {
+		Product product = productRepo.findById(productId)
+				.orElseThrow(() -> new RuntimeException("This product does not exist."));
+		return new ResponseEntity<Product>(product, HttpStatus.OK);
 	}
-	
-	@GetMapping(value ="/products/{productId}")
-	 public Product findById(@PathVariable("productId") Long productId) {
-		 for (Product prod: this.products) {
-			if (prod.getId().longValue()== productId.longValue()) {
-				return prod;
-			}
-		}
-		 return null;
-	 }
-	
+
 	@DeleteMapping(value = "/products/{productId}")
-		public void delete(@PathVariable("productId") Long productId) {
-		Product deleteProduct = null;
-		for (Product prod: this.products) {
-			if (prod.getId().longValue()== productId.longValue()) {
-				deleteProduct = prod;
-				break;
-			}
-		}
-		if (deleteProduct == null) {
-			throw new RuntimeException("This product does not exist");
-		}
-		this.products.remove(deleteProduct);
+	public ResponseEntity delete(@PathVariable("productId") Long productId) {
+		Product product = productRepo.findById(productId)
+				.orElseThrow(() -> new RuntimeException("This product does not exist."));
+		productRepo.delete(product);
+		return new ResponseEntity(HttpStatus.OK);
 	}
 }
