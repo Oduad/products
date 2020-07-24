4e13aea87b4106f9ff86abf14569c592cf125d2e
Oduad Virgilio Aragón Cruz <oduadaragon@gmail.com>
2020-07-23 23:49:34 -0500

    Creamos clase ProductDTO

---
+++ src/main/java/mx/products/products/dtos/ProductDTO.java
@@ -0,0 +1,33 @@
+package mx.products.products.dtos;
+
+public class ProductDTO {
+	
+	private Long id;
+	private String name;	
+	private Double price;
+	
+	public ProductDTO(Long id, String name, Double price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+}
