5d7950f4c943291d7881f20327a97a7ba266086c
Oduad Virgilio Aragón Cruz <oduadaragon@gmail.com>
2020-07-25 22:08:59 -0500

    Agregamos las clases NoDataFoundException GenerlServiceException y ValidateServiceException

---
+++ src/main/java/mx/products/products/exceptions/GenerlServiceException.java
@@ -0,0 +1,38 @@
+package mx.products.products.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//Siempre que no podamos controlar el error. Es un error inesperado que no sabemos de dónde viene.
+//RuntimeException lo podemos lanzar en cualquier lado.
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+public class GenerlServiceException extends RuntimeException{
+
+	public GenerlServiceException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public GenerlServiceException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GenerlServiceException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GenerlServiceException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GenerlServiceException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}	
+	
+}
