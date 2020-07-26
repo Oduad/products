afb7742b647da9315f4461bf16248c1d29ec39f0
Oduad Virgilio Aragón Cruz <oduadaragon@gmail.com>
2020-07-25 22:04:28 -0500

    Sustitución de RuntimeException por ValidateServiceException

--- src/main/java/mx/products/products/validators/ProductValidator.java
+++ src/main/java/mx/products/products/validators/ProductValidator.java
@@ -1,21 +1,22 @@
 package mx.products.products.validators;
 
 import mx.products.products.entity.Product;
+import mx.products.products.exceptions.ValidateServiceException;
 
 public class ProductValidator {
 
 	public static void save(Product product) {
 		if (product.getName()==null || product.getName().trim().isEmpty()) {
-			throw new RuntimeException("The name is required.");
+			throw new ValidateServiceException("The name is required.");
 		}
 		if (product.getName().length()>100) {
-			throw new RuntimeException("The name is too long.");
+			throw new ValidateServiceException("The name is too long.");
 		}
 		if (product. getPrice()==null) {
-			throw new RuntimeException("The price is required.");
+			throw new ValidateServiceException("The price is required.");
 		}
 		if (product. getPrice()<0) {
-			throw new RuntimeException("The price is incorrect.");
+			throw new ValidateServiceException("The price is incorrect.");
 		}
 	}
 	
